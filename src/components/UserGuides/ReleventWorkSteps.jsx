cf63dddb9798945acfbad60cc998aa48dca7302c
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-03-19 00:02:22 +0600

    User Guides Page Done

--- src/components/UserGuides/ReleventWorkSteps.jsx
+++ src/components/UserGuides/ReleventWorkSteps.jsx
@@ -1,44 +1,40 @@
 import { relevent_works_data } from "./Data";
 
+const ReleventWorkCard = ({ top, condition, index, data }) => {
+    const { image, text, link } = data;
+    return (
+        <div className="relative pb-10 w-full z-[1]">
+            <div
+                className={`relative flex justify-center w-full after:content-[''] after:absolute after:top-1/2 ${
+                    condition ? "after:left-1/2" : ""
+                } after:w-full after:h-[1px] after:border-b after:border-black after:inline-block after:-z-[1]`}
+            >
+                <img src={image} alt="" />
+            </div>
+            <div className="text-center mx-4">
+                <p className="mt-1">{top ? index + 1 : index + 7}</p>
+                <a href={link} className="text-sm">
+                    {text}
+                </a>
+            </div>
+        </div>
+    );
+};
+
 const ReleventWorkSteps = () => {
+    const top = relevent_works_data.slice(0, 6);
+    const bottom = relevent_works_data.slice(6, relevent_works_data.length);
+
     return (
         <>
-            <div className="flex justify-between ">
-                {relevent_works_data.slice(0, 6).map(({ image, text, link }, index) => (
-                    <div key={index} className="relative pb-10 min-w-[10rem] z-[1]">
-                        <div
-                            className={`relative flex justify-center w-full after:content-[''] after:absolute after:top-1/2 ${
-                                index === 0 ? "after:left-1/2" : ""
-                            } after:w-full after:h-[1px] after:border-b after:border-black after:inline-block after:-z-[1]`}
-                        >
-                            <img src={image} alt="" />
-                        </div>
-                        <div className="text-center mx-4">
-                            <p className="mt-1">{index + 1}</p>
-                            <a href={link} className="text-sm">
-                                {text}
-                            </a>
-                        </div>
-                    </div>
+            <div className="relative flex justify-between min-w-[56rem] after:absolute after:content-[''] after:min-w-[56rem] after:h-full after:border-r after:border-black after:top-6">
+                {top.map((data, i) => (
+                    <ReleventWorkCard key={i} index={i} data={data} condition={i === 0} top />
                 ))}
             </div>
-            <div className="flex flex-row-reverse justify-between">
-                {relevent_works_data.slice(6, relevent_works_data.length).map(({ image, text, link }, index) => (
-                    <div key={index} className="relative pb-10 min-w-[10rem] z-[1]">
-                        <div
-                            className={`relative flex justify-center w-full after:content-[''] after:absolute after:top-1/2 ${
-                                index === relevent_works_data.length - 7 ? "after:left-1/2" : ""
-                            } after:w-full after:h-[1px] after:border-b after:border-black after:inline-block after:-z-[1]`}
-                        >
-                            <img src={image} alt="" />
-                        </div>
-                        <div className="text-center mx-4">
-                            <p className="mt-1">{index + 7}</p>
-                            <a href={link} className="text-sm">
-                                {text}
-                            </a>
-                        </div>
-                    </div>
+            <div className="ml-16 flex flex-row-reverse justify-between min-w-[52rem]">
+                {bottom.map((data, i) => (
+                    <ReleventWorkCard key={i} index={i} data={data} condition={i === bottom.length - 1} />
                 ))}
             </div>
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
             md: "768px",
             lg: "1024px",
             xl: "1280px",
-            "2xl": "1600px",
         },
         fontFamily: {
             sans: ["Poppins", "sans-serif"],
